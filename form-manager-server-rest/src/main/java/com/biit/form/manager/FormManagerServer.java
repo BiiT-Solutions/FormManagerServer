0769d4b77c32d22367c589580cfa6a049e936ee3
Jorge Hortelano <jhortelano@biit-solutions.com>
2018-12-19 10:23:39 +0100

    Basic Springboot configuration

---
+++ form-manager-server-rest/src/main/java/com/biit/form/manager/FormManagerServer.java
@@ -0,0 +1,28 @@
+package com.biit.form.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class FormManagerServer {
+	private final static String SWAGGER_REST_LOCATION = "com.biit.form.manager.rest";
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormManagerServer.class, args);
+	}
+
+	@Bean
+	public Docket templateApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage(SWAGGER_REST_LOCATION)).paths(PathSelectors.any())
+				.build().alternateTypeRules(AlternateTypeRules.newRule(Class.class, String.class));
+	}
+}
