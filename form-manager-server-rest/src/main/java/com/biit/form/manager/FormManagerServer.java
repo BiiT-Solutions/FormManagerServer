d862f5db9807622f0094214bde1077f47ecc0ab3
Jorge Hortelano <jhortelano@biit-solutions.com>
2018-12-20 17:45:46 +0100

    Added annotations to applications

--- form-manager-server-rest/src/main/java/com/biit/form/manager/FormManagerServer.java
+++ form-manager-server-rest/src/main/java/com/biit/form/manager/FormManagerServer.java
@@ -2,7 +2,10 @@ package com.biit.form.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +15,9 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@ComponentScan({ "com.biit.form.manager", "com.biit.usermanager" })
+@EnableJpaRepositories(basePackages = "com.biit.usermanager")
+@EntityScan(basePackages = "com.biit.usermanager")
 @EnableSwagger2
 public class FormManagerServer {
 	private final static String SWAGGER_REST_LOCATION = "com.biit.form.manager.rest";
