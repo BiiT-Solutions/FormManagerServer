a04fedb1c169236647d64e761af1a1064cda9729
oferrando <oferrando@biit-solutions.com>
2019-01-10 20:13:02 +0100

    addedp uri params

--- form-manager-server-rest/src/main/java/com/biit/form/manager/rest/SecurityServices.java
+++ form-manager-server-rest/src/main/java/com/biit/form/manager/rest/SecurityServices.java
@@ -1,9 +1,11 @@
 package com.biit.form.manager.rest;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,20 +33,27 @@ public class SecurityServices {
 	@ApiOperation(value = "Basic method to check if the server is online.", notes = "The password is send in plain text. It is recommended the use of HTTPS for security reasons.")
 	@RequestMapping(value = "/security/user/{userName}/password/{password}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public String authorization(String userName, String password) throws InvalidUserException, InternalServerException {
+	public String authorization(
+			@ApiParam(value = "UserName", required = true) @PathVariable(value = "userName", required = true) String userName,
+			@ApiParam(value = "password", required = true) @PathVariable(value = "password", required = true) String password)
+			throws InvalidUserException, InternalServerException {
 		// try {
-			if(userName == "admin") {
-				return "{ user: req.body.username, token: 'wwwwwww' }";
-			} else {
-				throw new InvalidUserException("Invalid user '" + userName + "' or password incorrect.");
-				//return authenticationService.authenticate(userName, password);
-			}
-		/*} catch (UserManagementException | AuthenticationRequired e) {
-			FormManagerLogger.errorMessage(this.getClass().getName(), e);
-			throw new InternalServerException(e);
-		} catch (InvalidCredentialsException | UserDoesNotExistException e) {
-			FormManagerLogger.warning(this.getClass().getName(), "Invalid user '" + userName + "' with password '" + password + "'.");
-			throw new InvalidUserException("Invalid user '" + userName + "' or password incorrect.", e);
-		}*/
+
+		if (userName == "admin") {
+			return "{ user: req.body.username, token: 'wwwwwww' }";
+		} else {
+			throw new InvalidUserException("Invalid user '" + userName + "' or password incorrect.");
+			// return authenticationService.authenticate(userName, password);
+		}
+		/*
+		 * } catch (UserManagementException | AuthenticationRequired e) {
+		 * FormManagerLogger.errorMessage(this.getClass().getName(), e); throw new
+		 * InternalServerException(e); } catch (InvalidCredentialsException |
+		 * UserDoesNotExistException e) {
+		 * FormManagerLogger.warning(this.getClass().getName(), "Invalid user '" +
+		 * userName + "' with password '" + password + "'."); throw new
+		 * InvalidUserException("Invalid user '" + userName +
+		 * "' or password incorrect.", e); }
+		 */
 	}
 }
