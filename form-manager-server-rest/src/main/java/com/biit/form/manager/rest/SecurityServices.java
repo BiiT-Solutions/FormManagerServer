907f038ff5ce99cecb079d63ad2487a491a6f9bd
Óscar Ferrando <oferrando@biit-solutions.com>
2019-01-11 10:44:11 +0100

    uncoment

--- form-manager-server-rest/src/main/java/com/biit/form/manager/rest/SecurityServices.java
+++ form-manager-server-rest/src/main/java/com/biit/form/manager/rest/SecurityServices.java
@@ -14,6 +14,10 @@ import com.biit.form.manager.logger.FormManagerLogger;
 import com.biit.form.manager.rest.exceptions.InternalServerException;
 import com.biit.form.manager.rest.exceptions.InvalidUserException;
 import com.biit.usermanager.security.IAuthenticationService;
+import com.biit.usermanager.security.exceptions.AuthenticationRequired;
+import com.biit.usermanager.security.exceptions.InvalidCredentialsException;
+import com.biit.usermanager.security.exceptions.UserDoesNotExistException;
+import com.biit.usermanager.security.exceptions.UserManagementException;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -30,39 +34,39 @@ public class SecurityServices {
 	@ApiOperation(value = "Basic method to check if the server is online.", notes = "The password is send in plain text. It is recommended the use of HTTPS for security reasons.")
 	@RequestMapping(value = "/security/user/{userName}/password/{password}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public String authorization(
+	public void authorization(
 			@ApiParam(value = "UserName", required = true) @PathVariable(value = "userName", required = true) String userName,
 			@ApiParam(value = "password", required = true) @PathVariable(value = "password", required = true) String password)
 			throws InvalidUserException, InternalServerException {
-		// try {
+		try {
 
-		if (userName == "admin") {
-			return "{ user: req.body.username, token: 'wwwwwww' }";
-		} else {
-			throw new InvalidUserException("Invalid user '" + userName + "' or password incorrect.");
-			// return authenticationService.authenticate(userName, password);
+			if (userName == "admin") {
+				// return "{ user: req.body.username, token: 'wwwwwww' }";
+			} else {
+				authenticationService.authenticate(userName, password);
+				// throw new InvalidUserException("Invalid user '" + userName + "' or password incorrect.");			
+			}
+
+		} catch (UserManagementException | AuthenticationRequired e) {
+			FormManagerLogger.errorMessage(this.getClass().getName(), e);
+			throw new InternalServerException(e);
+		} catch (InvalidCredentialsException | UserDoesNotExistException e) {
+			FormManagerLogger.warning(this.getClass().getName(),
+					"Invalid user '" + userName + "' with password '" + password + "'.");
+			throw new InvalidUserException("Invalid user '" + userName + "' or password incorrect.", e);
 		}
-		/*
-		 * } catch (UserManagementException | AuthenticationRequired e) {
-		 * FormManagerLogger.errorMessage(this.getClass().getName(), e); throw new
-		 * InternalServerException(e); } catch (InvalidCredentialsException |
-		 * UserDoesNotExistException e) {
-		 * FormManagerLogger.warning(this.getClass().getName(), "Invalid user '" +
-		 * userName + "' with password '" + password + "'."); throw new
-		 * InvalidUserException("Invalid user '" + userName +
-		 * "' or password incorrect.", e); }
-		 */
+
 	}
 
 	@ApiOperation(value = "Basic method to check if the server is online.", notes = "")
 	@RequestMapping(value = "/security/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public String login(
-			@ApiParam(value = "", required = true) @RequestBody(required = true) LoginForm loginForm)
+	public String login(@ApiParam(value = "", required = true) @RequestBody(required = true) LoginForm loginForm)
 			throws InvalidUserException {
 		FormManagerLogger.info(this.getClass().getName(), "User " + loginForm.getUsername() + " succesfully logged in");
 		if (loginForm.getUsername().equals("admin")) {
-			FormManagerLogger.info(this.getClass().getName(), "User " + loginForm.getUsername() + " succesfully logged in");
+			FormManagerLogger.info(this.getClass().getName(),
+					"User " + loginForm.getUsername() + " succesfully logged in");
 			return "{ \"user\": \"admin\", \"token\": \"wwwwwww\" }";
 		} else {
 			throw new InvalidUserException("Invalid user '" + loginForm.getUsername() + "' or password incorrect.");
