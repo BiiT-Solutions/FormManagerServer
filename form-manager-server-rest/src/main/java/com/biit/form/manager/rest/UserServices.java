77b1b998176a2aa893a98588975c460543646d3e
Jorge Hortelano <jorgehortelano@gmail.com>
2019-01-13 11:07:31 +0100

    Created user services skeleton

---
+++ form-manager-server-rest/src/main/java/com/biit/form/manager/rest/UserServices.java
@@ -0,0 +1,38 @@
+package com.biit.form.manager.rest;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.biit.usermanager.repository.IUserRepository;
+
+/**
+ * Rest services for checking authorizations, permissions and logins.
+ */
+@RestController
+public class UserServices {
+
+	@Autowired
+	private IUserRepository userRepository;
+
+	@ApiOperation(value = "Basic method to add a new user to database.", notes = "Only one user can be added.")
+	@RequestMapping(value = "/user/add", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void addUser(@ApiParam(value = "User", required = true) @RequestBody(required = true) String requestBody) {
+
+	}
+
+	@ApiOperation(value = "Import a list of users created as CSV file", notes = "CSV structure as: Company; folder; username; password;")
+	@RequestMapping(value = "/user/import", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void importUsers(@ApiParam(value = "CSV file", required = true) @RequestBody(required = true) String requestBody) {
+
+	}
+}
