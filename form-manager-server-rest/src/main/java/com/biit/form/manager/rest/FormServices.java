2a46ddadb482bbbf1e5c40710e4a45bd2b8cd811
Jorge Hortelano <jhortelano@biit-solutions.com>
2019-01-14 15:23:11 +0100

    Storing forms into database

--- form-manager-server-rest/src/main/java/com/biit/form/manager/rest/FormServices.java
+++ form-manager-server-rest/src/main/java/com/biit/form/manager/rest/FormServices.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiParam;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,13 +18,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.biit.form.manager.entity.CompanyUser;
+import com.biit.form.manager.entity.FormDescription;
 import com.biit.form.manager.form.PdfConverter;
 import com.biit.form.manager.logger.FormManagerLogger;
+import com.biit.form.manager.repository.IFormDescriptionRepository;
+import com.biit.form.manager.rest.exceptions.DatabaseException;
 import com.biit.form.manager.rest.exceptions.InvalidFormException;
+import com.biit.form.manager.rest.exceptions.InvalidUserException;
 import com.biit.form.manager.rest.exceptions.PdfNotGeneratedException;
 import com.biit.form.result.FormResult;
+import com.biit.form.result.pdf.FormAsPdf;
 import com.biit.form.result.pdf.exceptions.EmptyPdfBodyException;
 import com.biit.form.result.pdf.exceptions.InvalidElementException;
+import com.biit.usermanager.repository.IUserRepository;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.lowagie.text.DocumentException;
@@ -31,11 +39,17 @@ import com.lowagie.text.DocumentException;
 @RestController
 public class FormServices {
 
+	@Autowired
+	private IUserRepository userRepository;
+
+	@Autowired
+	private IFormDescriptionRepository formDescriptionRepository;
+
 	@ApiOperation(value = "Basic method to save a form result from the formrunner.", notes = "")
 	@ResponseStatus(value = HttpStatus.OK)
 	@RequestMapping(value = "/forms", method = RequestMethod.POST, produces = MediaType.APPLICATION_PDF_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public byte[] saveFormResult(@ApiParam(value = "Form result", required = true) @RequestBody(required = true) String body) throws InvalidFormException,
-			PdfNotGeneratedException {
+			PdfNotGeneratedException, DatabaseException {
 		FormManagerLogger.info(this.getClass().getName(), "Form posted.");
 		try {
 			SubmittedForm submittedForm = parsePetition(body);
@@ -48,13 +62,22 @@ public class FormServices {
 			}
 
 			// Convert to PDF.
+			byte[] pdfContent;
 			try {
-				byte[] pdfContent = PdfConverter.convertToPdf(formResult);
+				pdfContent = PdfConverter.convertToPdf(formResult);
 				FormManagerLogger.info(this.getClass().getName(), "PDF for '" + submittedForm.getDocument() + "' created correctly.");
-				return pdfContent;
 			} catch (EmptyPdfBodyException | DocumentException | InvalidElementException e) {
 				throw new PdfNotGeneratedException("Pdf creation error.", e);
 			}
+
+			// Store it on database.
+			try {
+				storeOnDatabase(submittedForm);
+			} catch (InvalidUserException | EmptyPdfBodyException | DocumentException | InvalidElementException e) {
+				throw new DatabaseException("Form has not been stored into the database", e);
+			}
+
+			return pdfContent;
 
 		} catch (JsonSyntaxException e) {
 			throw new InvalidFormException("Structure invalid", e);
@@ -64,6 +87,27 @@ public class FormServices {
 	private SubmittedForm parsePetition(String petition) throws JsonSyntaxException {
 		FormManagerLogger.debug(this.getClass().getName(), petition);
 		return new Gson().fromJson(petition, SubmittedForm.class);
+	}
+
+	private void storeOnDatabase(SubmittedForm submittedForm) throws InvalidUserException, EmptyPdfBodyException, DocumentException, InvalidElementException,
+			InvalidFormException {
+		// Get user.
+		CompanyUser user = (CompanyUser) userRepository.findByLoginName(submittedForm.getName());
+		if (user == null) {
+			throw new InvalidUserException("No user exists with login name '" + submittedForm.getName() + "'.");
+		}
+		// Store form
+		FormDescription formDescription = new FormDescription(user, submittedForm.getJson());
+
+		// Convert to PDF.
+		FormResult formResult = FormResult.fromJson(submittedForm.getJson());
+		if (formResult == null) {
+			throw new InvalidFormException("Structure invalid");
+		}
+		FormAsPdf pdfDocument = new FormAsPdf(formResult);
+		formDescription.setPdfContent(pdfDocument.generate());
+
+		formDescriptionRepository.save(formDescription);
 	}
 
 	@ApiOperation(value = "Method to upload a file received as a multipart request", notes = "")
---
+++ form-manager-server-rest/src/main/java/com/biit/form/manager/rest/exceptions/DatabaseException.java
@@ -0,0 +1,22 @@
+package com.biit.form.manager.rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Unknown error.")
+public class DatabaseException extends Exception {
+	private static final long serialVersionUID = 2604760638951848101L;
+
+	public DatabaseException(String message) {
+		super(message);
+	}
+
+	public DatabaseException(String message, Throwable e) {
+		super(message, e);
+	}
+
+	public DatabaseException(Throwable e) {
+		super(e);
+	}
+
+}
