1c4602c1de2222f16bee70e4999755d18ca3545e
Jorge Hortelano <jhortelano@biit-solutions.com>
2019-01-15 16:59:59 +0100

    Using OpenCSV

--- form-manager-server-core/src/main/java/com/biit/form/manager/users/UserFactory.java
+++ form-manager-server-core/src/main/java/com/biit/form/manager/users/UserFactory.java
@@ -1,41 +1,50 @@
 package com.biit.form.manager.users;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
+import com.biit.form.manager.configuration.FormManagerConfigurationReader;
 import com.biit.form.manager.entity.CompanyUser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 public class UserFactory {
-	private final static int MIN_CSV_LINE_LENGTH = 10;
 
 	public static List<CompanyUser> createUsersFromCSV(String csvText) throws InvalidCsvFile {
 		List<CompanyUser> users = new ArrayList<>();
-		Scanner scanner = new Scanner(csvText);
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			if (line.length() < MIN_CSV_LINE_LENGTH || !line.contains(";")) {
-				continue;
-			}
-			String[] userFields = line.split(";");
-			if (userFields.length < UserCsvFields.length()) {
-				try {
-					throw new InvalidCsvFile("Line '" + line + "' is not well formed.");
-				} finally {
-					scanner.close();
+
+		try (Reader inputString = new StringReader(csvText)) {
+			try (CSVReader csvReader = new CSVReaderBuilder(inputString).withCSVParser(
+					new CSVParserBuilder().withSeparator(FormManagerConfigurationReader.getInstance().getCsvSeparator().charAt(0)).build()).build()) {
+				// Reading Records One by One in a String array
+				String[] userFields;
+				while ((userFields = csvReader.readNext()) != null) {
+					if (userFields.length <= 1) {
+						continue;
+					}
+					if (userFields.length < UserCsvFields.length()) {
+						throw new InvalidCsvFile("Line '" + Arrays.toString(userFields) + "' is not well formed.");
+					}
+					CompanyUser companyUser = new CompanyUser(userFields[UserCsvFields.USERNAME.getCsvIndex()].trim(),
+							userFields[UserCsvFields.PASSWORD.getCsvIndex()].trim(), createUniqueEmail(userFields[UserCsvFields.USERNAME.getCsvIndex()]), "",
+							"", userFields[UserCsvFields.COMPANY.getCsvIndex()].trim(), userFields[UserCsvFields.FOLDER.getCsvIndex()]);
+					users.add(companyUser);
 				}
 			}
-
-			CompanyUser companyUser = new CompanyUser(userFields[UserCsvFields.USERNAME.getCsvIndex()].trim(),
-					userFields[UserCsvFields.PASSWORD.getCsvIndex()].trim(), createUniqueEmail(userFields[UserCsvFields.USERNAME.getCsvIndex()]), "", "",
-					userFields[UserCsvFields.COMPANY.getCsvIndex()].trim(), userFields[UserCsvFields.FOLDER.getCsvIndex()]);
-			users.add(companyUser);
+		} catch (IOException e) {
+			throw new InvalidCsvFile("Error reading the CSV file", e);
 		}
-		scanner.close();
+
 		return users;
 	}
 
 	private static String createUniqueEmail(String userName) {
 		return userName + "@test.com";
 	}
+
 }
